a76d9aa978492b28d4c2ea5f4fa2c543d829b870
ESkeans <es42289@gmail.com>
2025-12-23 12:43:53 -0700

    Align cumulative and royalty chart heights

--- myproject/mapapp/static/mapapp/js/econ_charts.js
+++ myproject/mapapp/static/mapapp/js/econ_charts.js
@@ -243,7 +243,7 @@
       xaxis: {title: 'Period', type: 'category'},
       uniformtext: {minsize: 8, mode: 'hide'},
       template: 'plotly_white',
-      height: 400,
+      height: target.clientHeight || 400,
       margin: { l: 60, r: 20, t: 60, b: 60 }
     };
 
@@ -341,7 +341,7 @@
     };
 
     const layout = {
-      height: 400,
+      height: target.clientHeight || 400,
       title: {text: 'Value Loss Through Time', font: {size: 16, color: '#156082'}},
       margin: { l: 60, r: 20, t: 60, b: 60 },
       paper_bgcolor: '#ffffff',
